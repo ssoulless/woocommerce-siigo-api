f20cf0b71df0d1dc6ae409787d42259ee33b31d1
Sebastian Velandia <sebas.velandia27@gmail.com>
2021-03-18 12:52:06 -0500

    Added API authentication fields to admin settings

--- inc/Pages/Admin.php
+++ inc/Pages/Admin.php
@@ -71,54 +71,29 @@ class Admin extends BaseController
 		$args = array(
 			array(
 				'option_group' => 'woocommerce_siigo_api_plugin_authentication',
-				'option_name' => 'api_key',
+				'option_name' => 'nombre_clave_api',
+				'callback' => array( $this->callbacks, 'text_field_sanitize' )
+			),
+			array(
+				'option_group' => 'woocommerce_siigo_api_plugin_authentication',
+				'option_name' => 'usuario_api',
+				'callback' => array( $this->callbacks, 'text_field_sanitize' )
+			),
+			array(
+				'option_group' => 'woocommerce_siigo_api_plugin_authentication',
+				'option_name' => 'password_api',
+				'callback' => array( $this->callbacks, 'text_field_sanitize' )
+			),
+			array(
+				'option_group' => 'woocommerce_siigo_api_plugin_authentication',
+				'option_name' => 'subscription-key_api',
 				'callback' => array( $this->callbacks, 'text_field_sanitize' )
 			),
 			/*array(
 				'option_group' => 'woocommerce_siigo_api_plugin_settings',
 				'option_name' => 'quote_selection_preference',
 				'callback' => array( $this->callbacks, 'select_sanitize' )
-			),
-			array(
-				'option_group' => 'woocommerce_siigo_api_plugin_settings',
-				'option_name' => 'company_name',
-				'callback' => array( $this->callbacks, 'text_field_sanitize' )	
-			),
-			array(
-				'option_group' => 'woocommerce_siigo_api_plugin_settings',
-				'option_name' => 'company_first_name',
-				'callback' => array( $this->callbacks, 'text_field_sanitize' )	
-			),
-			array(
-				'option_group' => 'woocommerce_siigo_api_plugin_settings',
-				'option_name' => 'company_last_name',
-				'callback' => array( $this->callbacks, 'text_field_sanitize' )	
-			),
-			array(
-				'option_group' => 'woocommerce_siigo_api_plugin_settings',
-				'option_name' => 'company_email',
-				'callback' => array( $this->callbacks, 'email_field_sanitize' )	
-			),
-			array(
-				'option_group' => 'woocommerce_siigo_api_plugin_settings',
-				'option_name' => 'company_phone',
-				'callback' => array( $this->callbacks, 'text_field_sanitize' )	
-			),
-			array(
-				'option_group' => 'woocommerce_siigo_api_plugin_settings',
-				'option_name' => 'company_street',
-				'callback' => array( $this->callbacks, 'text_field_sanitize' )	
-			),
-			array(
-				'option_group' => 'woocommerce_siigo_api_plugin_settings',
-				'option_name' => 'company_crossstring',
-				'callback' => array( $this->callbacks, 'text_field_sanitize' )	
-			),
-			array(
-				'option_group' => 'woocommerce_siigo_api_plugin_settings',
-				'option_name' => 'company_suburb',
-				'callback' => array( $this->callbacks, 'text_field_sanitize' )	
-			)*/
+			),*/
 		);
 
 		$this->settings->set_settings( $args );
@@ -127,12 +102,6 @@ class Admin extends BaseController
 	public function set_sections()
 	{
 		$args = array(
-			/*array(
-				'id' => 'woocommerce_siigo_api_admin_company_info',
-				'title' => __('Dirección de Origen', 'envioclick'),
-				'callback' => array( $this->callbacks, 'woocommerce_siigo_api_company_info_section'),
-				'page' => 'woocommerce_siigo_api_plugin'
-			),*/
 			array(
 				'id' => 'woocommerce_siigo_api_admin_index',
 				'title' => __('Settings'),
@@ -154,29 +123,55 @@ class Admin extends BaseController
 	public function set_fields()
 	{
 		$args = array(
-			/*array(
-				'id' => 'shipping_quote_selection_preference',
-				'title' => __('¿Cómo se van a cotizar sus órdenes?', 'envioclick'),
-				'callback' => array( $this->callbacks, 'envioclick_quotation_preference_select'),
-				'page' => 'envioclick_plugin',
-				'section' => 'envioclick_admin_index',
-				'args' => array(
-					'label_for' => 'shipping_quote_selection_preference',
-					'class' => 'quotation_preferences'
-				)
-			),*/
 			array(
-				'id' => 'siigo_api_key',
-				'title' => 'API Key',
+				'id' => 'siigo_nombre_clave_api',
+				'title' => 'Nombre Clave',
 				'callback' => array( $this->callbacks, 'woocommerce_siigo_api_textfield'),
 				'page' => 'woocommerce_siigo_api_authentication',
 				'section' => 'woocommerce_siigo_api_authentication_index',
 				'args' => array(
-					'label_for' => 'siigo_api_key',
-					'class' => 'siigo_api_key'
-					'placeholder' => __('Pega la API key de Siigo aquí', 'woocommerce_siigo_api')
+					'label_for' => 'siigo_nombre_clave_api',
+					'class' => 'siigo_nombre_clave_api'
+					'placeholder' => __('Nombre clave de Siigo', 'woocommerce_siigo_api')
 				)
-			)
+			),
+			array(
+				'id' 	=> 'siigo_usuario_api',
+				'title' => 'Usuario Api',
+				'callback' => array( $this->callbacks, 'woocommerce_siigo_api_textfield' ),
+				'page' 	=> 'woocommerce_siigo_api_authentication',
+				'section'	=> 'woocommerce_siigo_api_authentication_index',
+				'args'	=> array( 
+					'label_for' => 'siigo_usuario_api',
+					'class'		=> 'siigo_usuario_api',
+					'placeholder' => __('Usuario API de Siigo', 'woocommerce_siigo_api')
+				)
+
+			),
+			array(
+				'id' 	=> 'siigo_password_api',
+				'title' => 'Password',
+				'callback' => array( $this->callbacks, 'woocommerce_siigo_api_textfield' ),
+				'page' 	=> 'woocommerce_siigo_api_authentication',
+				'section'	=> 'woocommerce_siigo_api_authentication_index',
+				'args'	=> array( 
+					'label_for' => 'siigo_password_api',
+					'class'		=> 'siigo_password_api',
+					'placeholder' => __('Password API de Siigo', 'woocommerce_siigo_api')
+				)
+			),
+			array(
+				'id' 	=> 'siigo_suscription_key_api',
+				'title' => 'Clave de suscripción de Siigo',
+				'callback' => array( $this->callbacks, 'woocommerce_siigo_api_textfield' ),
+				'page' 	=> 'woocommerce_siigo_api_authentication',
+				'section'	=> 'woocommerce_siigo_api_authentication_index',
+				'args'	=> array( 
+					'label_for' => 'siigo_suscription_key_api',
+					'class'		=> 'siigo_suscription_key_api',
+					'placeholder' => __('Clave de suscripción API de Siigo', 'woocommerce_siigo_api')
+				)
+			),
 			/*array(
 				'id' => 'company_name',
 				'title' => __('Nombre de la empresa', 'envioclick'),
@@ -188,103 +183,7 @@ class Admin extends BaseController
 					'class' => 'company_name'
 					'placeholder' => __('Nombre de la empresa', 'envioclick')
 				) 
-			),
-			array(
-				'id' => 'company_first_name',
-				'title' => __('Nombre'),
-				'callback' => array( $this->callbacks, 'envioclick_textfield'),
-				'page' => 'envioclick_plugin',
-				'section' => 'envioclick_admin_index',
-				'args' => array(
-					'label_for' => 'company_first_name',
-					'class' => 'company_first_name'
-					'placeholder' => __('Nombre del representante', 'envioclick')
-				) 
-			),
-			array(
-				'id' => 'company_last_name',
-				'title' => __('Apellido'),
-				'callback' => array( $this->callbacks, 'envioclick_textfield'),
-				'page' => 'envioclick_plugin',
-				'section' => 'envioclick_admin_index',
-				'args' => array(
-					'label_for' => 'company_last_name',
-					'class' => 'company_last_name'
-					'placeholder' => __('Apellido del representante', 'envioclick')
-				) 
-			),
-			array(
-				'id' => 'company_email',
-				'title' => __('Correo electrónico para las notificaciones de envioclick', 'envioclick'),
-				'callback' => array( $this->callbacks, 'envioclick_textfield'),
-				'page' => 'envioclick_plugin',
-				'section' => 'envioclick_admin_index',
-				'args' => array(
-					'label_for' => 'company_email',
-					'class' => 'company_email'
-					'placeholder' => __('Correo electrónico', 'envioclick')
-				) 
-			),
-			array(
-				'id' => 'company_phone',
-				'title' => __('Teléfono'),
-				'callback' => array( $this->callbacks, 'envioclick_textfield'),
-				'page' => 'envioclick_plugin',
-				'section' => 'envioclick_admin_index',
-				'args' => array(
-					'label_for' => 'company_phone',
-					'class' => 'company_phone'
-					'placeholder' => __('Teléfono', 'envioclick')
-				) 
-			),
-			array(
-				'id' => 'company_phone',
-				'title' => __('Teléfono'),
-				'callback' => array( $this->callbacks, 'envioclick_textfield'),
-				'page' => 'envioclick_plugin',
-				'section' => 'envioclick_admin_index',
-				'args' => array(
-					'label_for' => 'company_phone',
-					'class' => 'company_phone'
-					'placeholder' => __('Teléfono', 'envioclick')
-				) 
-			),
-			array(
-				'id' => 'company_street',
-				'title' => __('Dirección', 'envioclick'),
-				'callback' => array( $this->callbacks, 'envioclick_textfield'),
-				'page' => 'envioclick_plugin',
-				'section' => 'envioclick_admin_index',
-				'args' => array(
-					'label_for' => 'company_street',
-					'class' => 'company_street'
-					'placeholder' => __('Dirección', 'envioclick')
-				)
-			),
-			array(
-				'id' => 'company_crossstring',
-				'title' => __('Bloque/Torre Oficina/apto', 'envioclick'),
-				'callback' => array( $this->callbacks, 'envioclick_textfield'),
-				'page' => 'envioclick_plugin',
-				'section' => 'envioclick_admin_index',
-				'args' => array(
-					'label_for' => 'company_crossstring',
-					'class' => 'company_crossstring'
-					'placeholder' => __('Bloque/Torre Oficina/apto', 'envioclick')
-				)
-			),
-			array(
-				'id' => 'company_suburb',
-				'title' => __('Barrio/Localidad', 'envioclick'),
-				'callback' => array( $this->callbacks, 'envioclick_textfield'),
-				'page' => 'envioclick_plugin',
-				'section' => 'envioclick_admin_index',
-				'args' => array(
-					'label_for' => 'company_suburb',
-					'class' => 'company_suburb'
-					'placeholder' => __('Barrio/Localidad', 'envioclick')
-				)
-			)*/
+			),*/
 		);
 
 		$this->settings->set_fields( $args );
